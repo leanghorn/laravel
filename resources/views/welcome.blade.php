bb402df9954a95bd9e5ee56ac270cbf0c92a71e6
leanghorn <n.leanghorn@gmail.com>
2019-08-10 22:11:57 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel hello test 1
+                    Laravel hello test 2
                 </div>
 
                 <div class="links">
